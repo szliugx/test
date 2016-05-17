37d166254c86955276cac7424f750730df041dc4
szliugx <szliugx@gmail.com>
2016-05-17 13:27:36 +0000

    Yii日志相关

---
+++ Yii2日志记录相关/main.php
@@ -0,0 +1,37 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'defaultRoute' => 'index/index',
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning','info','profile'],
+                    'logFile' => '@app/runtime/logs/zlxoc/Thrift_api'.date("Y-m-d").'.log',//将日志按时间变量存储
+                    //'logVars' => ['_SERVER'],
+                    'logVars' => [],//此处是日志中不记录$_COOKIE和$_SERVER等超全局变量 ，默认记录$_COOKIE和$_SERVER
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
